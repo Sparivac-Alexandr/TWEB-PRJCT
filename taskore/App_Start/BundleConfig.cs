bc0f27629085ec4ee0d81679f2c8eb9b8b57a76a
Rotaru_Valentin <valentin.rotaru@isa.utm.md>
2025-02-27 13:58:57 +0200

    auth test

--- taskore/App_Start/BundleConfig.cs
+++ taskore/App_Start/BundleConfig.cs
@@ -21,7 +21,5 @@ namespace taskore.App_Start
 
 
         }
-
-
     }
 }
---
+++ taskore/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace taskore.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+    }
+}
