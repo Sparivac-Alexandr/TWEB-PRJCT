91d52dc68fd14b11c2267722807b7ef88b2411d2
mood_buster <hapiyergood@gmail.com>
2025-03-06 18:15:31 +0200

    redesign 4 pages

--- taskore/App_Start/BundleConfig.cs
+++ taskore/App_Start/BundleConfig.cs
@@ -6,23 +6,25 @@ using System.Web.Optimization;
 
 namespace taskore.App_Start
 {
-	public class BundleConfig
-	{
+	public class BundleConfig {
         public static void RegisterBundles(BundleCollection bundles) {
 
             //Style Bundle
             bundles.Add(new StyleBundle("~/Content/css").Include(
-     "~/Content/static/about.css",
-     "~/Content/static/auth-pages.css",
-      "~/Content/static/contact.css",
-      "~/Content/static/my-profile.css",
-      "~/Content/static/privacy.css",
-      "~/Content/static/terms.css"));
-
+     "~/wwwroot/about.css",
+     "~/wwwroot/auth-pages.css",
+      "~/wwwroot/contact.css",
+      "~/wwwroot/my-profile.css",
+      "~/wwwroot/privacy.css",
+      "~/wwwroot/terms.css",
+      "~/wwwroot/home.css",
+      "~/wwwroot/chat.css",
+      "~/wwwroot/explore-page.css",
+      "~/wwwroot/create-prjct.css"));
 
            //Script Bundle
             bundles.Add(new ScriptBundle("~/bundles/sitejs").Include(
-            "~/Content/static/site.js"));
+            "~/wwwroot/taskore.js"));
 
 
         }
